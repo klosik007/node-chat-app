8cbc649cb2e3dc0aa4136e57616843a40591279c
Przemyslaw Klos <pklos1992@gmail.com>
2019-02-18 19:14:02 +0100

    added user friendly socket usage

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,10 @@ socket.on('disconnect', function(){
 
 socket.on('newMessage', function (message){
     console.log('New message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
 });
 
 socket.emit('createMessage', {
@@ -23,3 +27,14 @@ socket.emit('createMessage', {
 }, function (message){
     console.log('got it', message);
 });
+
+jQuery('#message-form').on('submit', function (e){
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function(){
+
+    });
+})
