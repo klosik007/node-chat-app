4912b3c3782cd5b53dbc321ca4d637b402b1ff4f
Przemyslaw Klos <pklos1992@gmail.com>
2019-02-18 21:01:35 +0100

    Added geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', ()=>{
     it('should generate correct message object', ()=>{
@@ -8,6 +8,21 @@ describe('generateMessage', ()=>{
 
         expect(res.from).toBe('Przemek');
         expect(res.text).toBe('message');
-        expect(res.createdAt).toBeA('number');
+        expect(typeof res.createdAt).toBe('number');
+    });
+});
+
+describe('generateLocationMessage', ()=>{
+    it('should generate correct location object', ()=>{
+        //var message = generateLocationMessage('Admin', 1, 1);
+        var from = "Przemek";
+        var latitude = 12;
+        var longtitude = 15;
+        var url = 'https://www.google.com/maps?q=12,15';
+        var message = generateLocationMessage(from, latitude, longtitude);
+
+        //expect(message.url).toEqual('https://www.google.com/maps?q=1,1');
+        expect(typeof message.createdAt).toBe('number');
+        expect(message).toMatchObject({from, url});
     });
 });
