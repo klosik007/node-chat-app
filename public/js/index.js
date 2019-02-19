05a8d19599681b10f38c0655cce224e3d616ad22
Przemyslaw Klos <pklos1992@gmail.com>
2019-02-19 20:06:11 +0100

    added css styles

--- public/js/index.js
+++ public/js/index.js
@@ -42,11 +42,13 @@ socket.emit('createMessage', {
 jQuery('#message-form').on('submit', function (e){
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message');
+
     socket.emit('createMessage', {
         from: 'User',
         text: jQuery('[name=message]').val()
     }, function(){
-
+        messageTextbox.val('')
     });
 });
 
@@ -55,14 +57,17 @@ locationButton.on('click', function(){
     if(!navigator.geolocation){
         return alert('Geolocation not supported by your browser');
     }
+
+    locationButton.attr('disabled','disabled').text('Sending location...');
     
     navigator.geolocation.getCurrentPosition(function(position){
-        // console.log(position);
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longtitude: position.coords.longitude
         });
     }, function(){
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     });
 });
